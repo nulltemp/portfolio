eb79ab29f33d078ef57230622ef7d6e15460c236
nulltemp <nulltemp999@gmail.com>
2019-07-07 17:19:09 +0900

    ヘッダーのリンクからとぶと位置がずれる点の修正

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,15 +1,25 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center justify-center wrap>
-      <v-flex xs8>
-        <slot />
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="content">
+    <v-container>
+      <v-layout text-xs-center justify-center wrap>
+        <v-flex xs8>
+          <slot />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.content::before {
+  content: "";
+  display: block;
+  height: 5rem;
+  margin-top: -5rem;
+  visibility: hidden;
+}
+</style>
